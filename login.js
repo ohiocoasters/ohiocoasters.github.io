ca89f65871d3df79f14b0164c990d4bde2fe388d
Nicholas Gardner <nicholasgardner@nrcs.net>
2020-04-22 17:55:27 -0400

    login and clearnace conditonals set

--- login.js
+++ login.js
@@ -17,11 +17,23 @@ var uname = document.getElementById('email');
 var pwd = document.getElementById('password');
 
 function login(){
-if (uname == "nicholasgardner" && pwd == "352970");{
-    window.open("admindashboard.htm","_self");
+if (uname == "nicholasgardner" && pwd == "352970" || uname == "test" && pwd == "test"){
+    window.open("admindashboard.htm","_self")
 
 }
 
 
 }
 
+function autchkshp(){
+
+    if (uname == "nicholasgardner" && pwd == "352970") {
+        window.open("https://my.ecwid.com/store/28557208#dashboard:wizard=tiles","_self")
+    } else {
+        //  block of code to be executed if the condition1 is false and condition2 is false
+        alert("Sorry, You dont have sufficent clearance to view this page")
+        window.open("admindashboard.htm","_self")
+     
+      }
+
+    }
