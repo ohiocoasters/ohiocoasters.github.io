6091e9e34924973a86f502f8c849dcdf0e7b64fa
Nicholas Gardner <nicholasgardner@nrcs.net>
2020-04-23 20:45:07 -0400

    New home Page-new Shop -login edited-images added

--- login.js
+++ login.js
@@ -17,8 +17,8 @@ var uname = document.getElementById('email');
 var pwd = document.getElementById('password');
 
 function login(){
-if (uname == "nicholasgardner" && pwd == "352970" || uname == "test" && pwd == "test"){
-    window.open("admindashboard.htm","_self")
+if (uname == "nicholasgardner" && pwd == "352970" || uname == "test" && pwd == "test");{
+    window.open("https://www.ohiocoasters.github.io/admindashboard.htm","_self")
 
 }
 
@@ -30,7 +30,6 @@ function autchkshp(){
     if (uname == "nicholasgardner" && pwd == "352970") {
         window.open("https://my.ecwid.com/store/28557208#dashboard:wizard=tiles","_self")
     } else {
-        //  block of code to be executed if the condition1 is false and condition2 is false
         alert("Sorry, You dont have sufficent clearance to view this page")
         window.open("admindashboard.htm","_self")
      
